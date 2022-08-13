32d9dc4801a73271fec9e25ed231d7e1f11560e1
Raed-IT <RaedSwanIT@gmail.com>
2022-08-13 16:41:07 +0300

    relece

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*.{html,js}'],
+  content: ['./src/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {
       backgroundImage: {
@@ -14,5 +14,8 @@ module.exports = {
 
     },
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin'),
+    require('tailwindcss-rtl'),
+  ],
 }
